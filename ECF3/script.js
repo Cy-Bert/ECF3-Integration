3cb8678e36ef35f226250d495c92ce3709e0ab75
Ange0608 <angelique.dartevelle@hotmail.fr>
2023-08-30 09:01:12 +0200

    app done

--- ECF3/script.js
+++ ECF3/script.js
@@ -1,17 +1,26 @@
-document.querySelector("#like").onclick = function()
-{
+document.querySelector("#like").onclick = function () {
     document.querySelector("#emptyHeart").classList.toggle("hidden");
     document.querySelector("#fullHeart").classList.toggle("hidden");
 };
 
-document.querySelector('#buttonInfo').onclick = function()
-{
-    document.querySelector('#closeInfo').classList.toggle("hidden");
-    document.querySelector('#openInfo').classList.toggle("hidden");
+document.querySelector('#buttonInfo').onclick = function () {
+    let div = document.querySelector("#closeInfo");
+    div.classList.toggle("hidden");
+    div.animate(
+        [
+            //keyframes
+            { transform: 'translateY(100%' },
+            { transform: 'translateY(-20%' }, //Ajout d'un rebont
+            { transform: 'translateY(0' },
+        ],
+        {  //timing options
+            duration: 2000,
+            iterations: 1
+        }
+    )
+
 }
 
-document.querySelector('#closedArrow').onclick = function()
-{
+document.querySelector('#closedArrow').onclick = function () {
     document.querySelector('#closeInfo').classList.toggle("hidden");
-    document.querySelector('#openInfo').classList.toggle("hidden");
 }
